3bd8d4fb15ee9965634e23551fc654e518b77c77
omarrx14 <omarrx14@gmail.com>
2024-08-28 21:33:09 -0700

    Add files via upload

---
+++ src/components/grid.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './grid.css';
+
+const Grid = ({ rows, columns }) => {
+    return (
+        <div className="grid-container">
+            {Array.from({ length: rows }).map((_, rowIndex) => (
+                <div key={rowIndex} className="grid-row">
+                    {Array.from({ length: columns }).map((_, colIndex) => (
+                        <div key={colIndex} className="grid-cell"></div>
+                    ))}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Grid;
