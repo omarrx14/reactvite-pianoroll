3b2fa12a76e0b6b9697eb6f5c15326740eac08e1
omarrx14 <omarrx14@gmail.com>
2024-08-28 21:38:35 -0700

    fixed grid

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,17 +1,22 @@
 import './grid.css';
 
-const Grid = ({ rows, columns }) => {
-    return (
-        <div className="grid-container">
-            {Array.from({ length: rows }).map((_, rowIndex) => (
-                <div key={rowIndex} className="grid-row">
-                    {Array.from({ length: columns }).map((_, colIndex) => (
-                        <div key={colIndex} className="grid-cell"></div>
-                    ))}
-                </div>
-            ))}
+interface GridProps {
+  rows: number;
+  columns: number;
+}
+
+const Grid: React.FC<GridProps> = ({ rows, columns }) => {
+  return (
+    <div className="grid-container">
+      {Array.from({ length: rows }).map((_, rowIndex) => (
+        <div key={rowIndex} className="grid-row">
+          {Array.from({ length: columns }).map((_, colIndex) => (
+            <div key={colIndex} className="grid-cell"></div>
+          ))}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Grid;
